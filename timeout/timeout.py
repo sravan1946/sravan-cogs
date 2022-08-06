63c66bdb5904ae9632a696b4cf21dd528bd1b256
sravan <57430307+sravan1946@users.noreply.github.com>
2022-08-06 12:52:46 +0530

    so dont use Optional

--- timeout/timeout.py
+++ timeout/timeout.py
@@ -53,16 +53,12 @@ class Timeout(commands.Cog):
         self,
         ctx,
         member: discord.Member,
-        time: Optional[
-            (
-                TimedeltaConverter(
+        time: TimedeltaConverter(
                     minimum=datetime.timedelta(minutes=1),
                     maximum=datetime.timedelta(days=28),
                     default_unit="minutes",
                     allowed_units=["minutes", "seconds", "hours", "days"],
-                )
-            )
-        ] = None,
+                ) = None,
         *,
         reason: str = None,
     ):

564e36c145aa117c96abc37df400188466e6f945
{{ github.actor }} <sravan1946@users.noreply.github.com>
2022-08-06 07:20:53 +0000

    Style Reformatting

--- timeout/timeout.py
+++ timeout/timeout.py
@@ -53,12 +53,14 @@ class Timeout(commands.Cog):
         self,
         ctx,
         member: discord.Member,
-        time: Optional[TimedeltaConverter(
-            minimum=datetime.timedelta(minutes=1),
-            maximum=datetime.timedelta(days=28),
-            default_unit="minutes",
-            allowed_units=["minutes", "seconds", "hours", "days"],
-        )] = None,
+        time: Optional[
+            TimedeltaConverter(
+                minimum=datetime.timedelta(minutes=1),
+                maximum=datetime.timedelta(days=28),
+                default_unit="minutes",
+                allowed_units=["minutes", "seconds", "hours", "days"],
+            )
+        ] = None,
         *,
         reason: str = None,
     ):

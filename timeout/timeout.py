d33d5b62f83bad26447dc832c5126734ec3d3b87
{{ github.actor }} <sravan1946@users.noreply.github.com>
2023-04-29 08:59:28 +0000

    You messed up the style buddy. Lemme fix it for you

--- timeout/timeout.py
+++ timeout/timeout.py
@@ -4,7 +4,6 @@ from typing import List, Literal, Optional, Union
 
 import discord
 import humanize
-from discord.http import Route
 from discord.utils import utcnow
 from redbot.core import Config, commands, modlog
 from redbot.core.bot import Red

5044d7df7a74ed25fb413a3ee57f2668482e2345
sravan1946 <miniclip1956@gmail.com>
2024-03-24 14:53:17 +0530

    Add toggle for editrole

--- forcemention/forcemention.py
+++ forcemention/forcemention.py
@@ -26,8 +26,10 @@ SOFTWARE.
 # this is a modified version of Bobloy's forcemention cog
 # this is also a fork of phenom4n4n's forcemention cog
 
+import asyncio
+
 import discord
-from redbot.core import commands
+from redbot.core import Config, commands
 from redbot.core.bot import Red
 
 
@@ -36,16 +38,22 @@ class ForceMention(commands.Cog):
     Mention the unmentionables
     """
 
-    __version__ = "1.0.1"
+    __version__ = "1.1.0"
 
     def __init__(self, bot: Red):
         self.bot = bot
+        self.config = Config.get_conf(
+            self, identifier=634534234, force_registration=True
+        )
+        default_guild = {"editroles": False}
+        self.config.register_guild(**default_guild)
 
     async def red_delete_data_for_user(self, **kwargs):
         return
 
     @commands.bot_has_permissions(manage_roles=True)
     @commands.admin_or_permissions(mention_everyone=True)
+    @commands.guild_only()
     @commands.command("forcemention")
     async def cmd_forcemention(
         self, ctx: commands.Context, role: discord.Role, *, message: str = None
@@ -62,6 +70,37 @@ class ForceMention(commands.Cog):
             pass
         await self.forcemention(ctx.channel, role, message)
 
+    @commands.admin_or_permissions(manage_guild=True)
+    @commands.guild_only()
+    @commands.group("forcementionset")
+    async def forcementionset(self, ctx: commands.Context):
+        """
+        Settings for forcemention
+        """
+        pass
+
+    @forcementionset.command("editrole")
+    async def forcementionset_editrole(self, ctx: commands.Context, toggle: bool):
+        """
+        Toggle whether forcemention can edit roles
+        """
+        if toggle:
+            await ctx.send(
+                "Are you sure you want to allow me to edit roles? This could potentially be dangerous as this will edit the role to `mentionable` for a split second. Type **`yes`** to confirm."
+            )
+            try:
+                pred = await self.bot.wait_for(
+                    "message",
+                    check=lambda m: m.author == ctx.author and m.channel == ctx.channel,
+                    timeout=15,
+                )
+            except asyncio.TimeoutError:
+                return await ctx.send("Confirmation timed out.")
+            if pred.content.lower() != "yes":
+                return await ctx.send("Confirmation failed.")
+        await self.config.guild(ctx.guild).editroles.set(toggle)
+        await ctx.send(f"Role editing set to {toggle}")
+
     async def forcemention(
         self, channel: discord.TextChannel, role: discord.Role, message: str, **kwargs
     ):
@@ -73,8 +112,14 @@ class ForceMention(commands.Cog):
             and channel.permissions_for(me).manage_roles
             and me.top_role > role
         ):
-            await role.edit(mentionable=True)
-            await channel.send(message, allowed_mentions=mention_perms, **kwargs)
-            await role.edit(mentionable=False)
+            if await self.config.guild(channel.guild).editroles():
+                await role.edit(mentionable=True)
+                await channel.send(message, allowed_mentions=mention_perms, **kwargs)
+                await role.edit(mentionable=False)
+            else:
+                prefix = (await self.bot.get_prefix(channel))[0]
+                await channel.send(
+                    f"{role.mention} is unmentionable or I don't have `mention_everyone` permission in {channel.mention}. \nYou can allow me to edit roles by running `{prefix}forcementionset editrole true` so that I can mention it without `mention_everyone` permission."
+                )
         else:
             await channel.send(message, allowed_mentions=mention_perms, **kwargs)
